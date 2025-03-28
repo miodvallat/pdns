8f3288184a505565d43a471e27bc3928512f77ee
Peter van Dijk <peter.van.dijk@powerdns.com>
2025-03-28 07:04:35 +0100

    New DNSBackend entry points for views support.

--- pdns/dnsbackend.hh
+++ pdns/dnsbackend.hh
@@ -476,6 +476,36 @@ public:
     return false;
   }
 
+  virtual bool viewList(vector<string>& /* result */)
+  {
+    return false;
+  }
+
+  virtual bool viewListZones(const string& /* view */, vector<ZoneName>& /* result */)
+  {
+    return false;
+  }
+
+  virtual bool viewAddZone(const string& /* view */, const ZoneName& /* zone */)
+  {
+    return false;
+  }
+
+  virtual bool viewDelZone(const string& /* view */, const ZoneName& /* zone */)
+  {
+    return false;
+  }
+
+  virtual bool networkSet(const Netmask& /* net */, std::string& /* tag */)
+  {
+    return false;
+  }
+
+  virtual bool networkList(vector<pair<Netmask, string>>& /* networks */)
+  {
+    return false;
+  }
+
   //! Returns whether backend operations have caused files to be created.
   virtual bool hasCreatedLocalFiles() const
   {

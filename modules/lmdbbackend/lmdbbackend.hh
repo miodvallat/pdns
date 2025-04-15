0f64ddc83d2d02f756c700a4f62616f841155bf4
Peter van Dijk <peter.van.dijk@powerdns.com>
2025-04-15 15:13:09 +0200

    implement schema upgrade to v6

--- modules/lmdbbackend/lmdbbackend.hh
+++ modules/lmdbbackend/lmdbbackend.hh
@@ -171,6 +171,7 @@ public:
   // functions to use without constructing a backend object
   static std::pair<uint32_t, uint32_t> getSchemaVersionAndShards(std::string& filename);
   static bool upgradeToSchemav5(std::string& filename);
+  static bool upgradeToSchemav6(std::string& filename);
 
 private:
   struct compoundOrdername

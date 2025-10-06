02577c04eccf140f4c86d8b6cd311b5c3114e36b
Miod Vallat <miod.vallat@powerdns.com>
2025-10-06 14:39:04 +0200

    Structured logging for Bind backend.
    Signed-off-by: Miod Vallat <miod.vallat@powerdns.com>

--- modules/bindbackend/bindbackend2.hh
+++ modules/bindbackend/bindbackend2.hh
@@ -38,6 +38,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include "pdns/lock.hh"
+#include "pdns/logr.hh"
 #include "pdns/misc.hh"
 #include "pdns/dnsbackend.hh"
 #include "pdns/namespaces.hh"
@@ -256,10 +257,12 @@ private:
     bool get(DNSResourceRecord&);
     void reset();
 
-    handle();
+    handle() {}
 
     handle(const handle&) = delete;
     handle& operator=(const handle&) = delete; // don't go copying this
+
+    void setSLog(std::shared_ptr<Logr::Logger> log) { d_slog = log; }
 
     shared_ptr<const recordstorage_t> d_records;
     recordstorage_t::index<UnorderedNameTag>::type::const_iterator d_iter, d_end_iter;
@@ -273,6 +276,7 @@ private:
     QType qtype;
     bool d_list{false};
     bool mustlog{false};
+    std::shared_ptr<Logr::Logger> d_slog;
 
   private:
     bool get_normal(DNSResourceRecord&);
@@ -296,9 +300,11 @@ private:
   unique_ptr<SSqlStatement> d_deleteTSIGKeyQuery_stmt;
   unique_ptr<SSqlStatement> d_getTSIGKeysQuery_stmt;
 
+  std::shared_ptr<Logr::Logger> d_slog;
+  string d_logprefix;
+
   ZoneName d_transaction_qname;
   string d_transaction_tmpname;
-  string d_logprefix;
   set<string> alsoNotify; //!< this is used to store the also-notify list of interested peers.
   std::unique_ptr<ofstream> d_of;
   handle d_handle;
@@ -313,7 +319,7 @@ private:
 
   void queueReloadAndStore(domainid_t id);
   static bool findBeforeAndAfterUnhashed(std::shared_ptr<const recordstorage_t>& records, const DNSName& qname, DNSName& unhashed, DNSName& before, DNSName& after);
-  static void insertRecord(std::shared_ptr<recordstorage_t>& records, const ZoneName& zoneName, const DNSName& qname, const QType& qtype, const string& content, int ttl, const std::string& hashed = string(), const bool* auth = nullptr);
+  void insertRecord(std::shared_ptr<recordstorage_t>& records, const ZoneName& zoneName, const DNSName& qname, const QType& qtype, const string& content, int ttl, const std::string& hashed = string(), const bool* auth = nullptr);
   void reload() override;
   static string DLDomStatusHandler(const vector<string>& parts, Utility::pid_t ppid);
   static string DLDomExtendedStatusHandler(const vector<string>& parts, Utility::pid_t ppid);
@@ -321,6 +327,6 @@ private:
   static string DLReloadNowHandler(const vector<string>& parts, Utility::pid_t ppid);
   static string DLAddDomainHandler(const vector<string>& parts, Utility::pid_t ppid);
   static void fixupOrderAndAuth(std::shared_ptr<recordstorage_t>& records, const ZoneName& zoneName, bool nsec3zone, const NSEC3PARAMRecordContent& ns3pr);
-  static void doEmptyNonTerminals(std::shared_ptr<recordstorage_t>& records, const ZoneName& zoneName, bool nsec3zone, const NSEC3PARAMRecordContent& ns3pr);
+  void doEmptyNonTerminals(std::shared_ptr<recordstorage_t>& records, const ZoneName& zoneName, bool nsec3zone, const NSEC3PARAMRecordContent& ns3pr);
   void loadConfig(string* status = nullptr);
 };
